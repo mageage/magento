3f929739de962011c5c537d552cb46ac6a1d8b1b
Marceli Podstawski <marceli.podstawski@gmail.com>
2016-09-27 15:05:38 +0200

    Adding skeleton module for Guest_Wishlist

---
+++ app/code/local/MacPain/GuestWishlist/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @category   MacPain
+ * @package    MacPain_GuestWishlist
+ * @subpackage Helper
+ * @author     Marceli Podstawski <marceli.podstawski@gmail.com>
+ * @license    http://opensource.org/licenses/osl-3.0.php   Open Software License (OSL 3.0)
+ */
+class MacPain_GuestWishlist_Helper_Data
+    extends Mage_Core_Helper_Abstract
+{
+}
+	 
